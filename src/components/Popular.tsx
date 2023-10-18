c662d259e13ed785280382cf911ddd7fb9414018
subashhB <subashbaidya9@gmail.com>
2023-10-18 16:58:50 +0545

    style: Popular Anime

--- src/components/Popular.tsx
+++ src/components/Popular.tsx
@@ -3,6 +3,7 @@ import baseUrl from "../baseUrl";
 import axios from "axios";
 import { useAppSelector } from "../app/hooks";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 interface Anime {
   mal_id: number;
@@ -26,7 +27,6 @@ const Popular = () => {
     queryKey: ["popularAnime"],
     queryFn: fetchPopularAnime,
   });
-  console.log(popularAnime);
   const conditionalRender = () => {
     if (!isSearch) {
       return popularAnime?.data.map((anime) => {
@@ -38,7 +38,39 @@ const Popular = () => {
       });
     }
   };
-  return <div>{conditionalRender()}</div>;
+  return (
+    <PopularStyled>
+      <div className="popular-anime">{conditionalRender()}</div>
+    </PopularStyled>
+  );
 };
+
+const PopularStyled = styled.div`
+  display: flex;
+  .popular-anime: {
+    margin-top: 2rem;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+    padding-left: 5rem;
+    padding-right: 0;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-gap: 2rem;
+    background-color: #fff;
+    border-top: 5px solid #e5e7eb;
+    a {
+      height: 500px;
+      border-radius: 7px;
+      border: 5px solid #e5e7eb;
+    }
+    a img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      border-radius: 5px;
+    }
+  }
+`;
 
 export default Popular;
