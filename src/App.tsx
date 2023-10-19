35ff49a616045bc5c2f50864cc4392bb6af73ea4
subashhB <subashbaidya9@gmail.com>
2023-10-19 10:01:16 +0545

    feat: Browser Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import Popular from "./components/Popular";
+import { Route, Routes } from "react-router-dom";
+import AnimePage from "./pages/AnimePage";
+import Gallery from "./pages/Gallery";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
-      <Popular />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/anime/:id" element={<AnimePage />} />
+        <Route path="/character/:id" element={<Gallery />} />
+      </Routes>
     </>
   );
 }
